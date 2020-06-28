39f090f235818c7adb0e3edcf37b22267310cb2c
Nabil <nkoney14@outlook.com>
2020-06-28 17:11:54 -0400

    Add code to get image data from CIFAR10 dataset as well as two lines to compile and fit the model.

--- Image_Classifier.py
+++ Image_Classifier.py
@@ -1,8 +1,21 @@
+from keras.datasets import cifar10
+import keras.utils as utils
+
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.constraints import maxnorm
+from keras.optimizers import SGD
 
+# GETTING IMAGE DATA ############################################################################################
+labels_array = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+train_labels = utils.to_categorical(y_train)
+test_labels = utils.to_categorical(y_test)
+
+# BUILDING THE MODEL ############################################################################################
 # INITIALIZE THE MODEL
 # Sequential type lets us add layers IN ORDER
 model = Sequential()
@@ -48,4 +61,9 @@ model.add(Dropout(rate=0.5))
 #   1. units: however many categories you are returning
 #   2. activation
 model.add(Dense(units=10, activation='softmax'))
+
+# COMPILING MODEL #################################################################################################
+model.compile(optimizer=SGD(lr=.01), loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x=x_train, y=y_train, epochs=10, batch_size=32)
+
 
